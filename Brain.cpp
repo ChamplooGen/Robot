246b57fcac987838d91f62f8abac8ddcfe533f2c
ChamplooGen <alesvi96@gmail.com>
2016-02-18 21:32:48 +0300

    Create Brain.cpp

---
+++ Brain.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Cam.h"
+#include "Brain.h"
+#include "network.h"
+
+void Brain::start ()	// реализацмя функции
+{
+    std::cout << "I'm working!\n";
+    IEngine * left = Body::getInstance()->getLeftEngine();// Body::getInstance() получаем ссылку на единственный
+    left->Rotate('R', 58);
+    ICamera * camera = Body::getInstance()->getCamera();
+    camera->GetImage();
+    std::cout<<"\n";
+    Network::GetInstance()->Connect();
+    Network::GetInstance()->Get();
+    Network::GetInstance()->Listen();
+    Network::GetInstance()->Send();
+    Network::GetInstance()->Disconnect();
+
+
+
+
+
+//    while(commandBuilder.hasNextCommand()) {
+//        ICommand * cmd = commandBuilder.getNextCommand();
+//        cmd->execute();
+//    }
+} 
