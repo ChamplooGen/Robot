7851a6206c01d5c7295a5ef449ae86456a78247e
ChamplooGen <alesvi96@gmail.com>
2016-02-18 21:26:47 +0300

    Create Body.h

---
+++ Body.h
@@ -0,0 +1,25 @@
+#ifndef BODY_H
+#define BODY_H
+
+#include "ICam.h"
+#include "IEngine.h"
+#include "INet.h"
+
+class Body {
+private:
+    static Body * instance;
+    Body();
+	IEngine * leftEngine;
+	IEngine * rightEngine;
+	ICamera * camera;
+public:
+	~Body();
+    static Body * getInstance(); // для вызова этих функцмя не нужен объект
+    static void destroyInstance();
+
+	IEngine * getLeftEngine();
+	IEngine * getRightEngine();
+	ICamera * getCamera();
+};
+
+#endif
