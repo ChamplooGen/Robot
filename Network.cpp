e794707f8071184bbd75864024b8c1806816a5cb
ChamplooGen <alesvi96@gmail.com>
2016-03-16 00:48:27 +0300

    Update Network.cpp
    - Члены данные переведены в static;
    - Добавлен метод получения команды recievingCommand() от клиента;
    - Добавлена реализация создания socket'a для чтения данных от пользователя;

--- Network.cpp
+++ Network.cpp
@@ -1,56 +1,66 @@
-#include "network.h"
+#include "Network.h"
 #include <iostream>
 #include <QtWidgets>
 #include <QtNetwork>
 
 #include <stdlib.h>
 
+    QChar Network::keyWord = '0';
+    QChar Network::object = '0';
+    QChar Network::direction = '0';
+    quint16 Network::angle = 0;
 
 //Network * Network::instance = 0;
 
-        Network::Network()
-            :QObject()
-        {
+Network::Network() : QObject()
+{
+    qDebug() << "Building Network object";
+    // инициализация начальных данных
+    //keyWord = object = direction = '0';
+    //angle = 0;
 
-            tcpServer = new QTcpServer(this);
-            connect(tcpServer, SIGNAL(newConnection()), this, SLOT(onNewConnection()));
-            std::cout<<"Network was created.\n";
-        }
+    tcpServer = new QTcpServer(this);
+    connect(tcpServer, SIGNAL(newConnection()), this, SLOT(onNewConnection()));
+
+    qDebug() << "Network object created";
+}
 
 
-//        void Network::getCommand()
-//        {
-//            QDataStream in(tcpSocket);
+void Network::recievingCommand()
+{
+    QDataStream in(tcpSocket);
 
-//            if (blockSize == 0) // только начинаме чтение данных
-//            {
-//                if (tcpSocket->bytesAvailable() < (int)sizeof(quint16))
-//                    return;
-//                in >> blockSize;    // считали размер блока данных
-//            }
+    if (blockSize == 0) // только начинаме чтение данных
+    {
+        if (tcpSocket->bytesAvailable() < (int)sizeof(quint16))
+            return;
+        in >> blockSize;    // считали размер блока данных
+    }
 
-//            if (tcpSocket->bytesAvailable() < blockSize)
-//                return; // ждем, пока данные прийдут полностью;
-//            else blockSize = 0;
+    if (tcpSocket->bytesAvailable() < blockSize)
+        return; // ждем, пока данные прийдут полностью;
+    else blockSize = 0;
+    in >>  keyWord;
+    if(keyWord!='I')    // если команда не "Сфотографировать"
+    {
+        in >> object;
+        in >> direction;
+        in >> angle;
+    }
+    qDebug() <<"\nWe have a new command! Here it is:\n ("<<keyWord<<" , "<<object<<" , "<<direction<<", "<<angle<<")\n";
+}
 
-//            in >> keyWord;
-//            if(keyWord!='I')    // tсли команда не "Сфотографировать"
-//            {
-//                in >> object >> direction >> degrees;
-//            };
-//            std::cout <<"\nWe have a new command! Here it is:\n ("<<keyWord<<" , "<<object<<" , "<<direction<<", "<<degrees<<")\n";
 
-//        }
-\
+void Network::listen()
+{
+    tcpServer->listen(QHostAddress::LocalHost, 5100); // от 0 до 1000 идут системные порты
+    qDebug() << "Network object started listening";
+}
 
-        void Network::listen()
-        {
-            tcpServer->listen(QHostAddress::LocalHost, 5100); // от 0 до 1000 идут системные порты
-            std::cout<<"Network listen.\n";
-        }
-
-        void Network::onNewConnection()
-        {
-            std::cout<<"Here is a new connection!\n";
-        }
-// не менять*************************************
+void Network::onNewConnection()
+{
+    tcpSocket = tcpServer->nextPendingConnection();
+    connect(tcpSocket, SIGNAL(disconnected()), tcpSocket, SLOT(deleteLater(deleteLater())));
+    connect(tcpSocket, SIGNAL(readyRead()), tcpServer, SLOT(recievingCommand()));
+    qDebug() << "Network object accepted new command";
+}
