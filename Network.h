58ad581958faabdd171d7ad062e91f8a690425c7
ChamplooGen <alesvi96@gmail.com>
2016-03-16 00:45:27 +0300

    Update Network.h
    Поля команды и соответсвующие методы get'теры сделаны полями static (для возможности построения команд типа ICMD)

--- Network.h
+++ Network.h
@@ -7,28 +7,32 @@
 class Network: public QObject
 {
     Q_OBJECT
-
     private:
 
         QTcpServer *tcpServer;
         QTcpSocket *tcpSocket;
 
-
         quint16 blockSize;
     // элементы пустой команды
-        char keyWord;
-        char object;
-        char direction;
-        quint16 degrees;
+
+        static QChar keyWord;
+        static QChar object;
+        static QChar direction;
+        static quint16 angle;
 
     public:
-
         Network();
         void listen();
+        void getCommand() {recievingCommand();}
+
+        static QChar GetKeyWord() {return keyWord;}
+        static QChar GetObject() {return object;}
+        static QChar GetDirection() {return direction;}
+        static quint16 GetAngle() {return angle;}
 
     private slots:
         void onNewConnection();
-
+        void recievingCommand();
 };
 
 #endif // NETWORK_H
