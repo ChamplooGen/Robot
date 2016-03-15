42c9449aa1453605cdfcecb6f835d4d4760c678e
ChamplooGen <alesvi96@gmail.com>
2016-03-16 00:50:43 +0300

    Update CommandBuilder.cpp
    Добавлена реализация методов проверки наличия новой команды и получения этой команды.

--- CommandBuilder.cpp
+++ CommandBuilder.cpp
@@ -1,23 +1,35 @@
-#include "commandbuilder.h"
+#include "CommandBuilder.h"
 
 CommandBuilder::CommandBuilder()
 {
     network = new Network();
     network->listen();
+
+    qDebug() << "CommandBuilder object created";
 }
 
-bool CommandBuilder::HasNextCommand()
+bool CommandBuilder::HasNextCommand()   // не работает потому, что нет подключения
 {
-
+    if (network->GetKeyWord() == 'F') return false;
+    else return true;
+    //return false;   // !__NB__! создать функцию для опознавания того, что команда получена !__NB__!
 }
 
-ICMD *CommandBuilder::GetNextCommand()
+ICMD * CommandBuilder::GetNextCommand()
 {
-    return 0;
+    qDebug() <<"Trying to get new command";
+    network->getCommand();
+    qDebug() <<"Got it. Trying to build it";
+    CmdLeftEngine * cmd;
+    cmd = new CmdLeftEngine();
+    qDebug() <<"It was built";
+    return cmd;       // !__NB__! создать функцию для получения новой команды !__NB__!
 }
 
 
 CommandBuilder::~CommandBuilder()
 {
     delete network;
+
+    qDebug() << "CommandBuilder object destroyed";
 }
