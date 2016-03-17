2b413c0d4e9ac1514bf8038d72863b9cf571acc3
ChamplooGen <alesvi96@gmail.com>
2016-03-17 17:57:21 +0400

    Update CommandBuilder.cpp
    Изменен метод GetNextCommand() - теперь он возвращает команду для вращения левого двигателя только если ключевое слово команды - T, а объект команды - 'l', т.е. левый двигатель (см. протокол передачи данных).

--- CommandBuilder.cpp
+++ CommandBuilder.cpp
@@ -10,8 +10,8 @@ CommandBuilder::CommandBuilder()
 
 bool CommandBuilder::HasNextCommand()   // не работает потому, что нет подключения
 {
-    if (network->GetKeyWord() == 'F') return false;
-    else return true;
+    if (network->GetKeyWord() != 'F') return true;
+    else return false;
     //return false;   // !__NB__! создать функцию для опознавания того, что команда получена !__NB__!
 }
 
@@ -19,11 +19,13 @@ ICMD * CommandBuilder::GetNextCommand()
 {
     qDebug() <<"Trying to get new command";
     network->getCommand();
+    qDebug() << network->GetKeyWord().toLatin1() << " " << network->GetObject().toLatin1(); // строка проверка того, что в текущей команде
     qDebug() <<"Got it. Trying to build it";
     CmdLeftEngine * cmd;
     cmd = new CmdLeftEngine();
     qDebug() <<"It was built";
-    return cmd;       // !__NB__! создать функцию для получения новой команды !__NB__!
+    if (network->GetKeyWord() == 'T' && network->GetObject() == 'l') return cmd;       // !__NB__! создать функцию для получения новой команды !__NB__!
+    else return 0;
 }
 
 
